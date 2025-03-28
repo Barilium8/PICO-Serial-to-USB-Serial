d8406322ddb1a959bb430da42118fd6fcf036593
Steve Barile <Steve@ConductiveLabs.com>
2025-03-28 10:04:48 -0700

    Ver1.2 - refactored logic, updated menu-ing system, added persistent state., renamed to "Wave Terrain Synth".

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <Arduino.h>
 #include <pico/stdlib.h> // added to make UF2 button work, see SR1_BUTNUM_HELP
 #include <pico/bootrom.h> // added to make UF2 button work, see SR1_BUTNUM_HELP
+#include <EEPROM.h>
 
 // DMA can be used with PIO:
 // https://gregchadwick.co.uk/blog/playing-with-the-pico-pt2/
@@ -21,37 +22,18 @@
 #define PICO_DEFAULT_LED_GPIO 25
 #define HW_PROGRAM_BUTTON 26 // 26 new hw // was 2 // was 9
 
+#define ALL_BAUD 460'800
+
 // create USB_SERIAL (CDC) object
 Adafruit_USBD_CDC TinyUSB_SerialMIDI; // declare a USB 'Serial' port and wrap it in a MIDI intrerface
 struct USB_SM_Dev_Settings : public midi::DefaultSettings{
     static const bool UseRunningStatus = false;
     static const bool HandleNullVelocityNoteOnAsNoteOff = true;
     static const bool Use1ByteParsing = true;
-    static const long BaudRate = 460'800; // 31250;
+    static const long BaudRate = ALL_BAUD; //460'800; // 31250;
     static const unsigned SysExMaxSize = 64;
 };
 MIDI_CREATE_CUSTOM_INSTANCE(Adafruit_USBD_CDC, TinyUSB_SerialMIDI, MIDI_USB_SERIAL_DEV, USB_SM_Dev_Settings);
-
-// Creates MIDI on Serial1 aka UART0 (GPIO 0/1) to 'Loopback' MIDI to CM5
-struct MIDI_LoopBack_Settings : public midi::DefaultSettings{
-  static const bool UseRunningStatus = false;
-  static const bool HandleNullVelocityNoteOnAsNoteOff = true;
-  static const bool Use1ByteParsing = true;
-  static const long BaudRate = 460'800;
-  static const unsigned SysExMaxSize = 64;
-};
-MIDI_CREATE_CUSTOM_INSTANCE(HardwareSerial, Serial1, MIDI_CM5_UART0, MIDI_LoopBack_Settings);
-
-// Creates MIDI on Serial2 aka UART1 (GPIO 8/9) for PICO to ComputeModule MIDI "bus"
-struct MIDI_UART1_Settings : public midi::DefaultSettings{
-  static const bool UseRunningStatus = false;
-  static const bool HandleNullVelocityNoteOnAsNoteOff = true;
-  static const bool Use1ByteParsing = true;
-   // tested at 2'000'000 and works
-  static const long BaudRate = 460'800; //38'400; //31'250; //460'800; // 460800 baud tested output on PICO pin via analyser. Reqs 2MHz scan on the analyser! :-)
-  static const unsigned SysExMaxSize = 64;
-};
-MIDI_CREATE_CUSTOM_INSTANCE(HardwareSerial, Serial2, MIDI_PICO_UART1, MIDI_UART1_Settings);
 
 // create USB_MIDI object
 Adafruit_USBD_MIDI TinyUSB_MIDI; // declare a USB 'MIDI' port and wrap it in a MIDI intrerface
@@ -59,23 +41,59 @@ struct USBDev_Settings : public midi::DefaultSettings{
     static const bool UseRunningStatus = false;
     static const bool HandleNullVelocityNoteOnAsNoteOff = true;
     static const bool Use1ByteParsing = true;
-    static const long BaudRate = 460'800; // 31250;
+    static const long BaudRate = ALL_BAUD; // 460'800; // 31250;
     static const unsigned SysExMaxSize = 64;
 };
 MIDI_CREATE_CUSTOM_INSTANCE(Adafruit_USBD_MIDI, TinyUSB_MIDI, MIDI_USB_DEV, USBDev_Settings);
+
+
+// Creates MIDI on Serial1 aka UART0 (GPIO 0/1) connects to The WTS Controller (RP2040 on IO board)
+struct MIDI_CNTLR_Settings : public midi::DefaultSettings{
+  static const bool UseRunningStatus = false;
+  static const bool HandleNullVelocityNoteOnAsNoteOff = true;
+  static const bool Use1ByteParsing = true;
+  static const long BaudRate = ALL_BAUD; // tested at 2'000'000 and works
+  static const unsigned SysExMaxSize = 64;
+};
+MIDI_CREATE_CUSTOM_INSTANCE(HardwareSerial, Serial1, MIDI_PICO_UART0, MIDI_CNTLR_Settings);
+
+
+// Creates MIDI on Serial2 aka UART1 (GPIO 4/5) connects to CM5 (RaspPi Compute Module)
+struct MIDI_CM5_Settings : public midi::DefaultSettings{
+  static const bool UseRunningStatus = false;
+  static const bool HandleNullVelocityNoteOnAsNoteOff = true;
+  static const bool Use1ByteParsing = true;
+  static const long BaudRate = ALL_BAUD;
+  static const unsigned SysExMaxSize = 64;
+};
+MIDI_CREATE_CUSTOM_INSTANCE(HardwareSerial, Serial2, MIDI_CM5_UART1, MIDI_CM5_Settings);
 
 
 uint8_t theMIDIChan = 16;
 
 
 // function prototypes
-void MIDI_PICO_UART1_Get(); // UART1 Serial2 port
+void MIDI_PICO_UART0_Get(); // UART1 Serial2 port
 void MIDI_USB_SERIAL_DEV_Get();   // PC/MAC Serial port
 
 void SendControllerInfo();
 void BlinkLED(uint8_t times);
-bool gLoopBack = false;
-String version = "1.1";
+void DrawMenu();
+void EEPromUpdate(uint8_t addr, uint8_t val);
+
+bool gSend_PC_MIDI_As_MIDI = true;
+bool gPrevSend_PC_MIDI_As_MIDI = true;
+const uint8_t Addr_Send_PC_MIDI_As_MIDI = 1;
+
+bool gSend_PC_MIDI_As_SERIAL = true;
+bool gPrevSend_PC_MIDI_As_SERIAL = true;
+const uint8_t Addr_Send_PC_MIDI_As_SERIAL = 2;
+
+bool gSend_CM5_MIDI_As_SERIAL = false;
+bool gPrevSend_CM5_MIDI_As_SERIAL = false;
+const uint8_t Addr_Send_CM5_MIDI_As_SERIAL = 3;
+
+String version = "1.2";
 char formattedVer[46];
 
 //==============================================================================================
@@ -86,16 +104,43 @@ void setup() {
 
   pinMode(PICO_DEFAULT_LED_GPIO, OUTPUT);
 
-  gpio_init(HW_PROGRAM_BUTTON);
-  gpio_set_function(HW_PROGRAM_BUTTON, GPIO_FUNC_SIO);
-  gpio_set_dir(HW_PROGRAM_BUTTON, GPIO_IN);
-  pinMode(HW_PROGRAM_BUTTON, INPUT_PULLUP);
+  // gpio_init(HW_PROGRAM_BUTTON);
+  // gpio_set_function(HW_PROGRAM_BUTTON, GPIO_FUNC_SIO);
+  // gpio_set_dir(HW_PROGRAM_BUTTON, GPIO_IN);
+  // pinMode(HW_PROGRAM_BUTTON, INPUT_PULLUP);
+
+  //========= To Light WS2812 via PIO ============================
+
+  // stdio_init_all();
+
+  // PIO pio = pio0;
+  // int sm = 0;
+  // uint offset = pio_add_program(pio, &ws2812_program);
+  // uint8_t cnt = 0;
+
+  // puts("RP2040-Zero WS2812 Test");
+
+  // ws2812_program_init(pio, sm, offset, 16, 800000, true);
+
+  //====================================
+
+  delay (1000);
 
   // makes 45 wide string padded
-  sprintf(formattedVer, " Ver %-40s", version.c_str());
+  //sprintf(formattedVer, " Ver %-40s", version.c_str());
+  sprintf(formattedVer, " ver %-20s", version.c_str());
 
-  delay (500);
-  Serial.println(" PICO Serial-to-USB-Serial Starting... ");
+  #define EEPROM_SIZE 256
+  EEPROM.begin(EEPROM_SIZE);
+  gSend_PC_MIDI_As_MIDI = EEPROM.read(Addr_Send_PC_MIDI_As_MIDI);
+  gSend_PC_MIDI_As_SERIAL = EEPROM.read(Addr_Send_PC_MIDI_As_SERIAL);
+  gSend_CM5_MIDI_As_SERIAL = EEPROM.read(Addr_Send_CM5_MIDI_As_SERIAL);
+
+  gPrevSend_PC_MIDI_As_MIDI = gSend_PC_MIDI_As_MIDI;
+  gPrevSend_PC_MIDI_As_SERIAL = gSend_PC_MIDI_As_SERIAL;
+  gPrevSend_CM5_MIDI_As_SERIAL= gSend_CM5_MIDI_As_SERIAL;
+
+  DrawMenu();
 
 } // end setup
 
@@ -107,23 +152,23 @@ void setup1() {
   /* C:\Users\Steve\.platformio\packages\framework-arduinopico\libraries\Adafruit_TinyUSB_Arduino\src\arduino\ports\rp2040 */
 
   // Initialize Serial2 (UART1) for PICO-CM5 comms on GPIO8 (TX) and GPIO9 (RX)
-  uart_init(uart0, 460'800); // tested at 2'000'000 and works
+  uart_init(uart0, ALL_BAUD); // tested at 2'000'000 and works
   gpio_set_function(0, GPIO_FUNC_UART);  // Set GPIO0 to UART function
   gpio_set_function(1, GPIO_FUNC_UART);  // Set GPIO1 to UART function
 
   // Initialize Serial2 (UART1) for PICO-CM5 comms on GPIO8 (TX) and GPIO9 (RX)
-  uart_init(uart1, 460'800); // tested at 2'000'000 and works
-  gpio_set_function(8, GPIO_FUNC_UART);  // Set GPIO8 to UART function
-  gpio_set_function(9, GPIO_FUNC_UART);  // Set GPIO9 to UART function
+  uart_init(uart1, ALL_BAUD); // tested at 2'000'000 and works
+  gpio_set_function(4, GPIO_FUNC_UART);  // Set GPIO8 to UART function
+  gpio_set_function(5, GPIO_FUNC_UART);  // Set GPIO9 to UART function
 
-  MIDI_PICO_UART1.begin(MIDI_CHANNEL_OMNI);
-  MIDI_PICO_UART1.turnThruOff();
+  MIDI_PICO_UART0.begin(MIDI_CHANNEL_OMNI);
+  MIDI_PICO_UART0.turnThruOff();
 
-  TinyUSB_SerialMIDI.setStringDescriptor("SerialToUSBSerial");
+  TinyUSB_SerialMIDI.setStringDescriptor("Wave Terrain Synth Serial");
   MIDI_USB_SERIAL_DEV.begin(MIDI_CHANNEL_OMNI);
   MIDI_USB_SERIAL_DEV.turnThruOff();
 
-  TinyUSB_MIDI.setStringDescriptor("SerialToUSBSerial MIDI");
+  TinyUSB_MIDI.setStringDescriptor("Wave Terrain Synth");
   MIDI_USB_DEV.begin(MIDI_CHANNEL_OMNI);
   MIDI_USB_DEV.turnThruOff();
 
@@ -164,34 +209,55 @@ void loop() {
   if ( (now - prevTime3) > 500) {
     prevTime3 = millis();
 
-    if (tenTimes < 10) {
-      Serial.println(" Serial - Redirecting MIDI to MAC/PC (Serial).  Type '?' to see menu.");
-      tenTimes++;
-    }
-
     // TinyUSB_SerialMIDI.println("Serial - TinyUSB_SerialMIDI");
     // MIDI_USB_SERIAL_DEV.sendNoteOn(60,127,16);
     // MIDI_USB_SERIAL_DEV.sendNoteOff(60,127,16);
     // TinyUSB_SerialMIDI.println();
     String serialData = Serial.readString();
-    //Serial.println(String("Echo: ") + serialData);
-
-    if (serialData == "?") {
-      Serial.println("  o---------------------------------------------o");
-      Serial.println(String("  |") + formattedVer + "|");
-      //Serial.println("  |                                             |");
-      Serial.println("  |  C = PICO <-> CM5                           |");
-      Serial.println("  |  M = PICO <-> MAC/PC (Serial)               |");
-      Serial.println("  |  D = Print Debug message 10 more times      |");
-      Serial.println("  |  ? = this menu                              |");
-      Serial.println("  o---------------------------------------------o");
-      tenTimes = 10;
+    if (serialData == "M" || serialData == "m") {
+      DrawMenu();
     }
-    else if (serialData == "C") { gLoopBack = true;  Serial.println(" MIDI: PICO <-> CM5");}
-    else if (serialData == "M") { gLoopBack = false; Serial.println(" MIDI: PICO <-> MAC/PC (Serial)");}
-    else if (serialData == "D") { tenTimes = 0; }
-
+    else if (serialData == "1") {
+      gPrevSend_PC_MIDI_As_MIDI = gSend_PC_MIDI_As_MIDI;
+      gSend_PC_MIDI_As_MIDI = !gSend_PC_MIDI_As_MIDI;
+      EEPromUpdate(Addr_Send_PC_MIDI_As_MIDI, gSend_PC_MIDI_As_MIDI);
+      DrawMenu();
+      Serial.println("  ...toggled Send PC MIDI As MIDI");
+    }
+    else if (serialData == "2") {
+      gPrevSend_PC_MIDI_As_SERIAL = gSend_PC_MIDI_As_SERIAL;
+      gSend_PC_MIDI_As_SERIAL = !gSend_PC_MIDI_As_SERIAL;
+      EEPromUpdate(Addr_Send_PC_MIDI_As_SERIAL, gSend_PC_MIDI_As_SERIAL);
+      DrawMenu();
+      Serial.println("  ...toggled Send PC MIDI As Serial Data");
+    }
+    else if (serialData == "3") {
+      gPrevSend_CM5_MIDI_As_SERIAL = gSend_CM5_MIDI_As_SERIAL;
+      gSend_CM5_MIDI_As_SERIAL = !gSend_CM5_MIDI_As_SERIAL;
+      EEPromUpdate(Addr_Send_CM5_MIDI_As_SERIAL, gSend_CM5_MIDI_As_SERIAL);
+      DrawMenu();
+      Serial.println("  ...toggled Send CM5 MIDI As Serial Data");
+    }
   }
+
+    //=========== To Light WS2812 via PIO =========
+
+      // for (cnt = 0; cnt < 0xff; cnt++)
+      // {
+      //     put_rgb(cnt, 0xff - cnt, 0);
+      //     sleep_ms(3);
+      // }
+      // for (cnt = 0; cnt < 0xff; cnt++)
+      // {
+      //     put_rgb(0xff - cnt, 0, cnt);
+      //     sleep_ms(3);
+      // }
+      // for (cnt = 0; cnt < 0xff; cnt++)
+      // {
+      //     put_rgb(0, cnt, 0xff - cnt);
+      //     sleep_ms(3);
+      // }
+    //============================================
 
 } // end loop
 
@@ -204,7 +270,7 @@ void loop1() {
   if ( (micros() - prevTime1) > 800) { // 1000us 1ms
     prevTime1 = micros();
 
-    MIDI_PICO_UART1_Get(); // UART1 Serial2 port
+    MIDI_PICO_UART0_Get(); // UART1 Serial2 port
     MIDI_USB_SERIAL_DEV_Get(); // PC/MAC Serial port
   }
 } // end loop1
@@ -218,36 +284,36 @@ void HandleSysEx(byte *data, unsigned int length) {
 
 // =======================================================
 
-void MIDI_PICO_UART1_Get() {
-    if (MIDI_PICO_UART1.read()) {
-      if (gLoopBack) { // send to CM5
-        MIDI_CM5_UART0.send(MIDI_PICO_UART1.getType(), MIDI_PICO_UART1.getData1(), MIDI_PICO_UART1.getData2(), MIDI_PICO_UART1.getChannel()); // outbound to PC/MAC Serial port
-      }
-      else { // send to PC/MAC
-        MIDI_USB_SERIAL_DEV.send(MIDI_PICO_UART1.getType(), MIDI_PICO_UART1.getData1(), MIDI_PICO_UART1.getData2(), MIDI_PICO_UART1.getChannel()); // outbound to PC/MAC Serial port
-        MIDI_USB_DEV.send(MIDI_PICO_UART1.getType(), MIDI_PICO_UART1.getData1(), MIDI_PICO_UART1.getData2(), MIDI_PICO_UART1.getChannel());        // outbound to PC/MAC MIDI port
-      }
+void MIDI_PICO_UART0_Get() { // inbound from PICO Controller (via UART0)
+    if (MIDI_PICO_UART0.read()) {
+      if (gSend_CM5_MIDI_As_SERIAL)  MIDI_CM5_UART1.send(MIDI_PICO_UART0.getType(), MIDI_PICO_UART0.getData1(), MIDI_PICO_UART0.getData2(), MIDI_PICO_UART0.getChannel());      // outbound to PC/MAC Serial port
+      if (gSend_PC_MIDI_As_SERIAL)   MIDI_USB_SERIAL_DEV.send(MIDI_PICO_UART0.getType(), MIDI_PICO_UART0.getData1(), MIDI_PICO_UART0.getData2(), MIDI_PICO_UART0.getChannel()); // outbound to PC/MAC Serial port
+      if (gSend_PC_MIDI_As_MIDI)     MIDI_USB_DEV.send(MIDI_PICO_UART0.getType(), MIDI_PICO_UART0.getData1(), MIDI_PICO_UART0.getData2(), MIDI_PICO_UART0.getChannel());        // outbound to PC/MAC MIDI port
     }
 }
 
 // =======================================================
 
-void MIDI_USB_SERIAL_DEV_Get() { // inbound from PC/MAC Serial port (UART1)
-  if (!gLoopBack) {
-    if (MIDI_USB_SERIAL_DEV.read()) {
-      MIDI_PICO_UART1.send(MIDI_USB_SERIAL_DEV.getType(), MIDI_USB_SERIAL_DEV.getData1(), MIDI_USB_SERIAL_DEV.getData2(), MIDI_USB_SERIAL_DEV.getChannel()); // outbound to Controller
+void MIDI_CM5_UART1_Get() { // inbound from CM5 (via UART1)
+    if (MIDI_CM5_UART1.read() && gSend_CM5_MIDI_As_SERIAL) {
+      MIDI_PICO_UART0.send(MIDI_CM5_UART1.getType(), MIDI_CM5_UART1.getData1(), MIDI_CM5_UART1.getData2(), MIDI_CM5_UART1.getChannel()); // outbound to PICO Controller
     }
-  }
 }
 
 // =======================================================
 
-void MIDI_UART0_CM5_Get() { // inbound from CM5 Serial port (UART0)
-  if (gLoopBack) {
-    if (MIDI_CM5_UART0.read()) {
-      MIDI_PICO_UART1.send(MIDI_CM5_UART0.getType(), MIDI_CM5_UART0.getData1(), MIDI_CM5_UART0.getData2(), MIDI_CM5_UART0.getChannel()); // outbound to Controller
+void MIDI_USB_SERIAL_DEV_Get() { // inbound MIDI (as Serial Data) from PC/MAC (via USB)
+    if (MIDI_USB_SERIAL_DEV.read() && gSend_PC_MIDI_As_SERIAL) {
+      MIDI_PICO_UART0.send(MIDI_USB_SERIAL_DEV.getType(), MIDI_USB_SERIAL_DEV.getData1(), MIDI_USB_SERIAL_DEV.getData2(), MIDI_USB_SERIAL_DEV.getChannel()); // outbound to Controller
     }
-  }
+}
+
+// =======================================================
+
+void MIDI_USB_DEV_Get() { // inbound MIDI from PC/MAC (via USB)
+    if (MIDI_USB_SERIAL_DEV.read() && gSend_PC_MIDI_As_MIDI) {
+      MIDI_PICO_UART0.send(MIDI_USB_SERIAL_DEV.getType(), MIDI_USB_SERIAL_DEV.getData1(), MIDI_USB_SERIAL_DEV.getData2(), MIDI_USB_SERIAL_DEV.getChannel()); // outbound to Controller
+    }
 }
 
 //====================================================================
@@ -262,3 +328,59 @@ void BlinkLED(uint8_t times) {
       }
 
 } // end Blink LED
+
+//====================================================================
+
+void DrawMenu() {
+
+  Serial.print('\f'); // Clears screen
+  Serial.print("  WTS DEBUGGER (PICO) ");
+  Serial.println(formattedVer);
+  Serial.println();
+  Serial.println("  o---------------------------------------------o");
+  Serial.println("  |                                             |");
+
+  if (gSend_PC_MIDI_As_MIDI) {
+    Serial.println("  |  1 = Send PC MIDI as MIDI (is on)           |");
+  }
+  else{
+    Serial.println("  |  1 = Send PC MIDI as MIDI (is off)          |");
+  }
+
+  if (gSend_PC_MIDI_As_SERIAL) {
+    Serial.println("  |  2 = Send PC MIDI as Serial Data (is on)    |");
+  }
+  else{
+    Serial.println("  |  2 = Send PC MIDI as Serial Data (is off)   |");
+  }
+
+  Serial.println("  |                                             |");
+
+  if (gSend_CM5_MIDI_As_SERIAL) {
+    Serial.println("  |  3 = Send CM5 MIDI as Serial Data (is on)   |");
+  }
+  else{
+    Serial.println("  |  3 = Send CM5 MIDI as Serial Data (is off)  |");
+  }
+
+  Serial.println("  |                                             |");
+  Serial.println("  |  m = this menu                              |");
+  Serial.println("  |                                             |");
+  Serial.println("  o---------------------------------------------o");
+
+}
+
+//====================================================================
+
+void EEPromUpdate(uint8_t addr, uint8_t val) {
+  if (addr >= EEPROM_SIZE) {
+    return;
+  }
+
+  uint8_t tempVal = EEPROM.read(addr);
+  if (tempVal != val) {
+    EEPROM.write(addr, val);
+    EEPROM.commit();
+  }
+
+}
